ce4e838e08d2148b0c2dcef1aaffed096f1493d4
David Nuño <morfeo_david@hotmail.com>
2019-01-14 21:09:35 +0100

    Firestore integrado

--- IkasFit19/app/src/main/java/org/morfe/ikasfit19/Principal.java
+++ IkasFit19/app/src/main/java/org/morfe/ikasfit19/Principal.java
@@ -3,8 +3,12 @@ package org.morfe.ikasfit19;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class Principal extends AppCompatActivity {
 
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
