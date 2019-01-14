c13e90e0a0d4bee6a55426834983aff98b6f59fc
David Nuño <morfeo_david@hotmail.com>
2019-01-14 21:24:28 +0100

    guarda en firestore

--- IkasFit19/app/src/main/java/org/morfe/ikasfit19/Principal.java
+++ IkasFit19/app/src/main/java/org/morfe/ikasfit19/Principal.java
@@ -5,13 +5,29 @@ import android.os.Bundle;
 
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Principal extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-    
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("nombre", "David");
+        user.put("edad", "33");
+        user.put("apellidos", "Nuño");
+
+// Add a new document with a generated ID
+        db.collection("usuarios")
+                .add(user);
+
     }
 }
