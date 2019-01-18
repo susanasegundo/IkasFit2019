97f65ffc8bb63a86c43853c1f8f49d663ab133c0
Unknown <davidnunovelez@gmail.com>
2019-01-18 20:44:18 +0100

    20:44 18012019

--- IkasFit19/app/src/main/java/org/morfe/ikasfit19/Ventanas/Principal.java
+++ IkasFit19/app/src/main/java/org/morfe/ikasfit19/Ventanas/Principal.java
@@ -1,29 +1,21 @@
 package org.morfe.ikasfit19.Ventanas;
 
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.data.DataPoint;
@@ -31,18 +23,13 @@ import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
 import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.data.Value;
-import com.google.android.gms.fitness.request.DataReadRequest;
 import com.google.android.gms.fitness.request.DataSourcesRequest;
 import com.google.android.gms.fitness.request.OnDataPointListener;
 import com.google.android.gms.fitness.request.SensorRequest;
 import com.google.android.gms.fitness.result.DataSourcesResult;
 
-import org.morfe.ikasfit19.BaseDatos.BaseDatos;
-import org.morfe.ikasfit19.Clases.GetCaloriesAsyncTask;
 import org.morfe.ikasfit19.R;
 
-import java.text.SimpleDateFormat;
-import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 
