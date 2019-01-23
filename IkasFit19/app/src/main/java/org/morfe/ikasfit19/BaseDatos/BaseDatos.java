8ff34c8ac03a44c425ebc1178ed3a2b99cb7c475
David Nuño <morfeo_david@hotmail.com>
2019-01-23 20:37:59 +0100

    Login anónimo Funciona.
    Google fit funciona en algunos móviles

--- IkasFit19/app/src/main/java/org/morfe/ikasfit19/BaseDatos/BaseDatos.java
+++ IkasFit19/app/src/main/java/org/morfe/ikasfit19/BaseDatos/BaseDatos.java
@@ -1,11 +1,13 @@
 package org.morfe.ikasfit19.BaseDatos;
 
+
 import android.support.annotation.NonNull;
 import android.util.Log;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,40 +16,36 @@ import static android.content.ContentValues.TAG;
 public class BaseDatos  {
 
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
-
     public BaseDatos() {
+
     }
-
-
-    public boolean guardarUsuario(){
+    public boolean guardarUsuario( FirebaseAuth mAuth){
 
         // Create a new user with a first and last name
         Map<String, Object> user = new HashMap<>();
-        user.put("fecha", "15/01/2020");
-        user.put("pasosTotales", 3000);
-        user.put("posicionRanking", 2);
+        user.put("fecha", "21/01/2020");
+        user.put("pasosTotales", 50000);
+
 
         // Add a new document with a generated ID
-        db.collection("usuarios")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+        db.collection("usuarios").document(mAuth.getUid())
+                .set(user)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
-                    public void onSuccess(DocumentReference documentReference) {
-                        Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
-
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
                     }
                 })
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "Error adding document", e);
+                        Log.w(TAG, "Error writing document", e);
                     }
                 });
 
         return true;
     }
-
-    public boolean bucarUsuario(){
-        return true;
-    }
 }
+
+
+
--- IkasFit19/app/src/main/java/org/morfe/ikasfit19/Ventanas/Principal.java
+++ IkasFit19/app/src/main/java/org/morfe/ikasfit19/Ventanas/Principal.java
@@ -3,11 +3,9 @@ package org.morfe.ikasfit19.Ventanas;
 
 import android.content.Intent;
 import android.content.IntentSender;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,11 +13,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.Scopes;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Scope;
 import com.google.android.gms.common.api.Status;
+
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.data.DataPoint;
 import com.google.android.gms.fitness.data.DataSource;
@@ -31,9 +28,27 @@ import com.google.android.gms.fitness.request.OnDataPointListener;
 import com.google.android.gms.fitness.request.SensorRequest;
 import com.google.android.gms.fitness.result.DataSourcesResult;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.morfe.ikasfit19.BaseDatos.BaseDatos;
 import org.morfe.ikasfit19.R;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
+
+import static android.content.ContentValues.TAG;
 
 
 
@@ -41,6 +56,7 @@ public class Principal extends AppCompatActivity implements OnDataPointListener,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener {
 
+    private FirebaseAuth mAuth;
     private static final int REQUEST_OAUTH = 1;
     private static final String AUTH_PENDING = "auth_state_pending";
     private boolean authInProgress = false;
@@ -48,49 +64,66 @@ public class Principal extends AppCompatActivity implements OnDataPointListener,
 
     private static Button boton;
     private static TextView textView;
-
+    private BaseDatos bd;
 
     @Override
     protected void onStart() {
         super.onStart();
         mApiClient.connect();
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
     }
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_principal);
+        mAuth = FirebaseAuth.getInstance();
         boton = (Button) findViewById(R.id.button);
         textView = (TextView) findViewById(R.id.textoMostrar);
+        setContentView(R.layout.activity_principal);
+
         if (savedInstanceState != null) {
             authInProgress = savedInstanceState.getBoolean(AUTH_PENDING);
         }
-
         mApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Fitness.SENSORS_API)
-                .addApi(Fitness.HISTORY_API)
-                .addScope(Fitness.SCOPE_ACTIVITY_READ)
-                .addScope(new Scope(Scopes.FITNESS_ACTIVITY_READ))
+                .addScope(Fitness.SCOPE_ACTIVITY_READ_WRITE) //new Scope(Scopes.FITNESS_ACTIVITY_READ_WRITE))
                 .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .build();
 
-        //BaseDatos bd = new BaseDatos();
-        //bd.guardarUsuario();
+        mAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInAnonymously:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
 
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+                            Toast.makeText(Principal.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
 
+                        }
+
+                        // ...
+                    }
+                });
+        bd = new BaseDatos();
+        bd.guardarUsuario(mAuth);
     }
 
     private void registerFitnessDataListener(DataSource dataSource, DataType dataType) {
 
         SensorRequest request = new SensorRequest.Builder()
                 .setDataSource( dataSource )
-                .setDataType( dataType )
+                .setDataType(dataType)
                 .setSamplingRate( 3, TimeUnit.SECONDS )
                 .build();
-
-
 
         Fitness.SensorsApi.add( mApiClient, request, this )
                 .setResultCallback(new ResultCallback<Status>() {
@@ -101,14 +134,13 @@ public class Principal extends AppCompatActivity implements OnDataPointListener,
                         }
                     }
                 });
-
     }
 
     @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        Log.d("GoogleFit","Conectado");
+    public void onConnected(Bundle bundle) {
+
         DataSourcesRequest dataSourceRequest = new DataSourcesRequest.Builder()
-                .setDataTypes( DataType.TYPE_STEP_COUNT_CUMULATIVE, DataType.AGGREGATE_STEP_COUNT_DELTA )
+                .setDataTypes( DataType.TYPE_STEP_COUNT_CUMULATIVE )
                 .setDataSourceTypes( DataSource.TYPE_RAW )
                 .build();
 
@@ -125,7 +157,6 @@ public class Principal extends AppCompatActivity implements OnDataPointListener,
 
         Fitness.SensorsApi.findDataSources(mApiClient, dataSourceRequest)
                 .setResultCallback(dataSourcesResultCallback);
-
     }
 
     @Override
@@ -151,14 +182,12 @@ public class Principal extends AppCompatActivity implements OnDataPointListener,
 
     @Override
     public void onDataPoint(DataPoint dataPoint) {
-        Log.d("GoogleFit","Llegan datos");
         for( final Field field : dataPoint.getDataType().getFields() ) {
             final Value value = dataPoint.getValue( field );
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     Toast.makeText(getApplicationContext(), "Field: " + field.getName() + " Value: " + value, Toast.LENGTH_SHORT).show();
-
                 }
             });
         }
@@ -184,6 +213,24 @@ public class Principal extends AppCompatActivity implements OnDataPointListener,
         textView.setText("Mierda");
 
     }
+    @Override
+    protected void onStop() {
+        super.onStop();
 
+        Fitness.SensorsApi.remove( mApiClient, this )
+                .setResultCallback(new ResultCallback<Status>() {
+                    @Override
+                    public void onResult(Status status) {
+                        if (status.isSuccess()) {
+                            mApiClient.disconnect();
+                        }
+                    }
+                });
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(AUTH_PENDING, authInProgress);
+    }
 
 }
